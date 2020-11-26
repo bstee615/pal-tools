4f61b4ac56ab6aca96368bdbe87c492a611d4d81
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2020-11-26 10:50:15 -0600

    Get all struct definitions

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int sum(int x, char y)
+struct f
 {
-    int a = x + y;
+    int x;
+    char c;
+}
+
+int sum(struct f myf, char y)
+{
+    int a = myf.x + y;
     return a;
 }
 
 int main()
 {
+    struct f myf;
+    myf.x = 3;
     int s = sum(3, 4);
     printf("Hello, world! %d\n", s);
 }
--- parse.py
+++ parse.py
@@ -17,13 +17,24 @@ def find_funcdefs(node):
     Return all function declarations
     """
 
-    if node.location.file and node.location.file.name == 'main.c':
-        if node.kind == CursorKind.FUNCTION_DECL:
-            print(f'function decl {pp(node)}]')
-            yield node
+    if node.kind == CursorKind.FUNCTION_DECL:
+        # print(f'function decl {pp(node)}]')
+        yield node
     # Recurse for children of this node
     for child in node.get_children():
         yield from find_funcdefs(child)
+
+def find_structs(node):
+    """
+    Return all structs
+    """
+
+    if node.kind == CursorKind.STRUCT_DECL:
+        # print(f'struct decl {pp(node)}]')
+        yield node
+    # Recurse for children of this node
+    for child in node.get_children():
+        yield from find_structs(child)
 
 def main():
     filename = 'main.c'
@@ -34,8 +45,12 @@ def main():
 
     cur = tu.cursor
     funcdefs = find_funcdefs(cur)
-    last_funcdef = max(funcdefs, key=lambda n: n.location.line)
+    last_funcdef = max(funcdefs, key=lambda n: n.location.file.name == filename and n.location.line)
     print(f'last: {pp(last_funcdef)}')
+
+    structs = find_structs(cur)
+    for s in filter(lambda n: n.location.file.name == filename, structs):
+        print(pp(s))
 
 if __name__ == "__main__":
     main()
