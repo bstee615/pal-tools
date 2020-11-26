208298dd80a769f0da7b08e8114c33205f29cdee
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2020-11-26 11:49:12 -0600

    Refactor to generic find method

--- parse.py
+++ parse.py
@@ -12,40 +12,19 @@ def pp(node):
     """
     return f'{node.displayname} ({node.kind}) [{node.location}]'
 
-def find_funcdecls(node):
+def find(node, kind, verbose=False):
     """
-    Return all function declarations
+    Return all node's descendants of a certain kind
     """
 
-    if node.kind == CursorKind.FUNCTION_DECL:
+    if verbose:
+        print(pp(node))
+
+    if node.kind == kind:
         yield node
     # Recurse for children of this node
     for child in node.get_children():
-        yield from find_funcdecls(child)
-
-def find_structs(node):
-    """
-    Return all struct decls
-    """
-
-    if node.kind == CursorKind.STRUCT_DECL:
-        yield node
-    # Recurse for children of this node
-    for child in node.get_children():
-        yield from find_structs(child)
-
-def find_parmdecls(node):
-    """
-    Return all parameter decls in function decl
-    """
-
-    # print(pp(node))
-
-    if node.kind == CursorKind.PARM_DECL:
-        yield node
-    # Recurse for children of this node
-    for child in node.get_children():
-        yield from find_parmdecls(child)
+        yield from find(child, kind)
 
 def main():
     filename = 'main.c'
@@ -56,17 +35,17 @@ def main():
     
     cur = tu.cursor
 
-    structs = find_structs(cur)
+    structs = find(cur, CursorKind.STRUCT_DECL)
     spellings_structs = ((n.spelling, n) for n in structs)
     structs_by_spelling = dict(spellings_structs)
     print(pp(structs_by_spelling['f']))
 
-    funcdefs = find_funcdecls(cur)
+    funcdefs = find(cur, CursorKind.FUNCTION_DECL)
     last_funcdef = max(funcdefs, key=lambda n: n.location.file.name == filename and n.location.line)
     print(f'last: {pp(last_funcdef)}')
 
-    for i in find_parmdecls(last_funcdef):
-        print(pp(i))
+    for child in find(last_funcdef, CursorKind.PARM_DECL):
+        print(pp(child))
 
 if __name__ == "__main__":
     main()
