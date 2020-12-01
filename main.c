3958133d3882dbe5f4daab823da650103723493e
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2020-12-01 17:14:31 -0600

    Swap sum and body in README.md

--- main.c
+++ main.c
@@ -8,16 +8,19 @@ struct f
     char *cp;
 };
 
-int sum(int x, unsigned int y)
-{
-    unsigned int z = x+y;
-    printf("%d + %u = %u\n", x, y, z);
-    return z;
-}
+int sum(int x, unsigned int y);
+int body(struct f *myf);
 
 int body(struct f *myf)
 {
     printf("params: %d %u %c %s\n", myf->x, myf->y, myf->c, myf->cp);
     int z = sum(myf->x, myf->y);
+    return z;
+}
+
+int sum(int x, unsigned int y)
+{
+    unsigned int z = x+y;
+    printf("%d + %u = %u\n", x, y, z);
     return z;
 }
