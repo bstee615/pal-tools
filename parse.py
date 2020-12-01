a32659a55f9d7bd25bca07bd0c997af88f4a9b86
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2020-12-01 14:44:43 -0600

    Add argparse, output formatting

--- parse.py
+++ parse.py
@@ -6,6 +6,11 @@
 import clang.cindex
 from clang.cindex import CursorKind, TypeKind
 
+import argparse
+import shutil
+import subprocess
+import os
+
 def pp(node):
     """
     Return str of node for pretty print
@@ -50,9 +55,9 @@ def local_vars(type, varname):
         if type.spelling == 'char *':
             yield LocalVariable(type, varname, 0)
         else:
-        # TODO: Currently inits all ptrs as single values. What about arrays?
-        yield from local_vars(type.get_pointee(), f'{varname}_v')
-        yield LocalVariable(type, varname, 1)
+            # TODO: Currently inits all ptrs as single values. What about arrays?
+            yield from local_vars(type.get_pointee(), f'{varname}_v')
+            yield LocalVariable(type, varname, 1)
     elif type.kind == TypeKind.INT:
         yield LocalVariable(type, varname)
     elif type.kind == TypeKind.UINT:
@@ -103,8 +108,8 @@ free({str_name});
             if v.type.spelling == 'char *':
                 return throwaway_getline(v.name, '{0} = malloc({2});\nstrcpy({0}, {1});')
             else:
-            # TODO: Currently inits all ptrs as single values. What about arrays?
-            return f'{v.name} = &{vars_so_far[-v.children].name};'
+                # TODO: Currently inits all ptrs as single values. What about arrays?
+                return f'{v.name} = &{vars_so_far[-v.children].name};'
         elif v.type.kind == TypeKind.INT:
             return throwaway_getline(v.name, '{0} = atoi({1});')
         elif v.type.kind == TypeKind.UINT:
@@ -129,43 +134,89 @@ def codegen(fn_name, param_names, inits):
 
 int main() {{
 // BEGIN declare input variables
-    {joiner.join(decls)}
+{joiner.join(decls)}
 // END declare input variables
 
 // BEGIN read input variables
-    {joiner.join(defs)}
+{joiner.join(defs)}
 // END read input variables
 
 // BEGIN call into segment
-    {fn_name}({", ".join(param_names)});
+{fn_name}({", ".join(param_names)});
 // END call into segment
 }}
 // END test harness
 '''
-    )
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('filename', help="Path to the input file")
+    parser.add_argument('-o', "--output", help="Path to the output file", type=str, nargs=1)
+    parser.add_argument('-f', "--format", help="Format the output file with clang-format", action="store_true")
+
+    return parser.parse_args()
 
 def main():
-    filename = 'main.c'
+    args = get_args()
+    infile = args.filename
+    outfile = args.output[0] if args.output else None
+    should_format = args.format
 
     index = clang.cindex.Index.create()
-    tu = index.parse(filename)
+    tu = index.parse(infile)
     print('translation unit:', tu.spelling)
     
     cur = tu.cursor
 
     funcdecls = find(cur, CursorKind.FUNCTION_DECL)
-    target = max(funcdecls, key=lambda n: n.location.file.name == filename and n.location.line)
+    target = max(funcdecls, key=lambda n: n.location.line if n.spelling != 'main' and n.location.file.name == infile else -1)
     print(f'target function: {pp(target)}')
 
     inits = []
     parmesan = list(find(target, CursorKind.PARM_DECL))
-    for parm in parmesan:
+    print(f'target function has {len(parmesan)} parameters')
+    for i, parm in enumerate(parmesan):
         locals = list(local_vars(parm.type, parm.displayname))
         this_boy_inits = list(initializers(locals))
         inits += this_boy_inits
+        print(f'parameter {i} {pp(parm)} produces {len(locals)} local variables')
+        for l in locals:
+            print(f'local variable {l}')
 
     param_names = (p.displayname for p in parmesan)
-    codegen(target.spelling, param_names, inits)
+    test_harness = codegen(target.spelling, param_names, inits)
+
+    with open(infile, 'r') as f:
+        text = f.read()
+    raw_text = text + '\n' + test_harness
+
+    if outfile:
+        with open(outfile, 'w') as f:
+            f.write(raw_text)
+        
+        if should_format:
+            if shutil.which('clang-format'):
+                subprocess.check_call(['clang-format', outfile, '-i'])
+            else:
+                print('WARNING: Requested format but clang-format not found')
+    else:
+        print('generated test harness:')
+        if should_format:
+            if shutil.which('clang-format'):
+
+                tmp_filename = '/tmp/parse.py.fmt.c'
+                with open(tmp_filename, 'w') as f:
+                    f.write(raw_text)
+                subprocess.check_call(['clang-format', tmp_filename, '-i'])
+                with open(tmp_filename, 'r') as f:
+                    formatted_text = f.read()
+                os.remove(tmp_filename)
+
+                print(formatted_text)
+            else:
+                print('WARNING: Requested format but clang-format not found')
+        else:
+            print(raw_text)
 
 if __name__ == "__main__":
     main()
