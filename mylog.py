8a59592a3f5be82b84c00836f6aca7db123b3419
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2020-12-11 13:03:22 -0600

    Move logging to toolkit

---
+++ mylog.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+"""
+Common logger
+"""
+
+log = logging.getLogger()
+stdout_handler = logging.StreamHandler(sys.stdout)
+verbose_fmt = logging.Formatter('%(levelname)s - %(message)s')
+stdout_handler.setFormatter(verbose_fmt)
+log.addHandler(stdout_handler)
+log.setLevel(logging.DEBUG)
