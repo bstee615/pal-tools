9c4b5a82994edf638c6fb02bc14dc25e19229a25
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-25 14:05:51 -0600

    Print vardecls, case and default stmts

--- tools/trace/trace.py
+++ tools/trace/trace.py
@@ -3,11 +3,14 @@
 from mylog import log
 import argparse
 import logging
+import nodeutils
+from clang.cindex import CursorKind
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--log-level', help='Display logs at a certain level (DEBUG, INFO, ERROR)')
     parser.add_argument('-v', '--verbose', action='store_true', help='Display verbose logs in -lDEBUG')
+    parser.add_argument('-p', '--pin-root', help='Use an alternative path to Pin root.')
     arguments = parser.parse_args()
     
     if arguments.log_level:
@@ -24,7 +27,10 @@ args = parse_args()
 verbose=False
 
 def main():
-    print('TODO traceify the things')
+    root = nodeutils.parse('test.c')
+    vd = nodeutils.find(root, CursorKind.VAR_DECL) + nodeutils.find(root, CursorKind.CASE_STMT) + nodeutils.find(root, CursorKind.DEFAULT_STMT)
+    for v in vd:
+        log.debug(nodeutils.pp(v))
 
 if __name__ == '__main__':
     main()
