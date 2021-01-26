6440624e78c92666206d08bde687c1c07f0fb274
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-26 11:05:00 -0600

    Add test for Pin install script and fix some errors

--- tools/trace/install.sh
+++ tools/trace/install.sh
@@ -1,13 +1,23 @@
 # Download Pin 3.16
+if [ -z "$1" ]
+then
+    rename_to='pin-3.16'
+else
+    rename_to="$(basename $1)"
+fi
+
 pin_url='https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.16-98275-ge0db48c31-gcc-linux.tar.gz'
-rename_to="$1" || 'pin-3.16'
 tools="$rename_to/source/tools"
 if [ ! -d $rename_to ]
 then
     # Download and unzip Pin
     echo Downloading and unpacking from $pin_url...
     wget -qO- $pin_url | tar --transform "s/^pin-3.16-98275-ge0db48c31-gcc-linux/$rename_to/" -xvz
-    test -d $rename_to || (echo Could not download Pin from $pin_url to $rename_to. && exit 1)
+    if [ ! $? -eq 0 -a -d $rename_to ]
+    then
+        echo Could not download Pin from $pin_url to $rename_to.
+        exit 1
+    fi
     # Delete all tools except the support necessary for trace-pintool
     ls -d $tools/* | grep -v -e trace-pintool -e Config -e Utils -e makefile | xargs rm -r
 else
@@ -15,13 +25,18 @@ else
 fi
 
 # Clone trace-pintool
+trace_url='https://github.com/bstee615/trace-pintool'
 trace_tool="$tools/trace-pintool"
 if [ ! -d $trace_tool ]
 then
     echo Cloning pintool repo at $trace_tool...
-    trace_url='https://github.com/bstee615/trace-pintool'
     git clone $trace_url $trace_tool
-    test -d $trace_tool || (echo Could not clone trace-pintool from $trace_url to $trace_tool. && exit 1)
+    if [ ! $? -eq 0 -a -d $trace_tool ]
+    then
+        echo echo Could not clone trace-pintool from $trace_url to $trace_tool.
+        exit 1
+    fi
+    rm -rf "$trace_tool/.git"
 else
     echo Pintool already exists at $trace_tool.
 fi
@@ -31,7 +46,7 @@ pushd $trace_tool
 make
 make_ret=$?
 popd
-if [[ $make_ret != 0 ]]
+if [ ! $make_ret -eq 0 ]
 then
     echo Could not build Pin. Please cd to $trace_tool and run make.
     exit 1
